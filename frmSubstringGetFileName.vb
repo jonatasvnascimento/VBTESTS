ed3ffec85e277980ff9c0d890a87a4a46b1b5a66
Jonatas Viana <56093381+jonatasvnascimento@users.noreply.github.com>
2024-06-05 15:29:31 -0300

    Updated btnCortar_Click method with new variables and string concatenation
    Updated the `btnCortar_Click` method with several changes. Added new variables `nameFile` and `posicaoUltimaBarra` to handle file name and position extraction from `nomeFileSave`. Introduced a new hardcoded FTP link string `link` and its unescaped version `linkDesescapado`. Updated `txbResposta.Text` property to concatenate `nameFile`, a substring of `nomeFileSave`, and `linkDesescapado`. Error handling code remains unchanged.

--- frmSubstringGetFileName.vb
+++ frmSubstringGetFileName.vb
@@ -8,10 +8,32 @@
             Dim nameFile = nomeFileSave.substring(nomeFileSave.LastIndexOf("/") + 1)
             Dim posicaoUltimaBarra As Integer = nomeFileSave.LastIndexOf("/"c) + 1
 
+            Dim link As String = "ftp://Kjonatas@192.168.90.92/Call%20teste/2024/06/05/11/Putting On The Ritz - Freedom Trail Studio.mp3"
+
+            Dim linkDesescapado As String = Uri.UnescapeDataString(link)
+            Dim posicaoUltimaBarras As Integer = linkDesescapado.LastIndexOf("/"c)
+            Dim caminhoFTP As String = linkDesescapado.Substring(0, posicaoUltimaBarras + 1)
+            Dim nomeArquivo As String = linkDesescapado.Substring(posicaoUltimaBarras + 1)
+
+            Dim link1 As String = "C:\Users\jonat\Downloads\teste.mp3"
+            Dim linkDesescapado1 As String = Uri.UnescapeDataString(link1)
+            Dim posicaoUltimaBarras1 As Integer = linkDesescapado1.LastIndexOf("\"c)
+            Dim caminhoFTP1 As String = linkDesescapado1.Substring(0, posicaoUltimaBarras1 + 1)
+            Dim nomeArquivo1 As String = linkDesescapado1.Substring(posicaoUltimaBarras1 + 1)
+
+
 
             txbResposta.Text = nameFile
             txbResposta.Text += Environment.NewLine
             txbResposta.Text += nomeFileSave.substring(0, posicaoUltimaBarra)
+            txbResposta.Text += Environment.NewLine
+            txbResposta.Text += caminhoFTP
+            txbResposta.Text += Environment.NewLine
+            txbResposta.Text += nomeArquivo
+            txbResposta.Text += Environment.NewLine
+            txbResposta.Text += caminhoFTP1
+            txbResposta.Text += Environment.NewLine
+            txbResposta.Text += nomeArquivo1
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try

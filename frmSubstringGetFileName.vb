578e7934b98a8029655312c94053ca70ac188e40
Jonatas Viana <56093381+jonatasvnascimento@users.noreply.github.com>
2024-06-05 13:33:02 -0300

    Updated `nomeFileSave` value and enhanced `txbResposta` update
    Updated the value of `nomeFileSave` to a new FTP path. Added code to calculate the position of the last slash in the `nomeFileSave` string using the `LastIndexOf` method. Enhanced the update of the `txbResposta` text box by setting the text to the `nameFile` string, adding a new line, and appending the substring of `nomeFileSave` up to the last slash.

--- frmSubstringGetFileName.vb
+++ frmSubstringGetFileName.vb
@@ -1,12 +1,17 @@
 ﻿Public Class frmSubstringGetFileName
-    Dim nomeFileSave = "ftp://Kjonatas@192.168.90.92/teste/Body of Water - TrackTribe.mp3"
+    Dim nomeFileSave = "ftp://Kjonatas@192.168.90.92/2024/06/05/11/Putting On The Ritz - Freedom Trail Studio.mp3"
     Private Sub frmSubstringGetFileName_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         txbEnvio.Text = nomeFileSave
     End Sub
     Private Sub btnCortar_Click(sender As Object, e As EventArgs) Handles btnCortar.Click
         Try
             Dim nameFile = nomeFileSave.substring(nomeFileSave.LastIndexOf("/") + 1)
+            Dim posicaoUltimaBarra As Integer = nomeFileSave.LastIndexOf("/"c) + 1
+
+
             txbResposta.Text = nameFile
+            txbResposta.Text += Environment.NewLine
+            txbResposta.Text += nomeFileSave.substring(0, posicaoUltimaBarra)
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
